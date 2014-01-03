a11677852e95a06174d1030e083e5403df29f34f
Brady Garvin <bgarvin@acm.org>
2014-01-03 10:57:53 -0600

    Added a couple monoids that will be useful factors for a delimiter monoid, which in turn will be useful for matching and jit-parsing across delimiters.

---
+++ paralleling_monoid.hpp
@@ -0,0 +1,76 @@
+#ifndef PARALLELING_MONOID_HEADER
+#define PARALLELING_MONOID_HEADER
+
+#include <cassert>
+
+#include "monoid_sequence.hpp"
+
+/* A paralleling monoid for T is (monoid_sequence<T>::iterator with a fresh
+ * zero, max, zero).  It is useful as a factor monoid when we elements in one
+ * monoid sequence to appear in the same order as corresponding elements in
+ * another.
+ */
+template<typename T>class paralleling_monoid {
+protected:
+  using iterator_type = typename monoid_sequence<T>::iterator;
+  iterator_type*			iterator;
+
+public:
+  paralleling_monoid(unsigned zero) :
+    iterator{nullptr} {
+    assert(!zero);
+  }
+  paralleling_monoid(const iterator_type&iterator) :
+    iterator{new iterator_type{iterator}} {}
+  paralleling_monoid(const paralleling_monoid&copy) :
+    iterator{copy.iterator ?
+      new iterator_type{*copy.iterator} : nullptr} {}
+
+  ~paralleling_monoid() {
+    if (iterator) {
+      delete iterator;
+    }
+  }
+
+  paralleling_monoid&operator =(const paralleling_monoid&other) {
+    if (&other == this) {
+      return *this;
+    }
+    if (iterator) {
+      delete iterator;
+    }
+    iterator = other.iterator ?
+      new iterator_type{*other.iterator} : nullptr;
+  }
+
+  operator iterator_type() {
+    assert(iterator);
+    return *iterator;
+  }
+  operator const iterator_type() const {
+    assert(iterator);
+    return *iterator;
+  }
+
+  paralleling_monoid operator +(const paralleling_monoid&other) const {
+    return (*this < other) ? other : *this;
+  }
+  paralleling_monoid&operator +=(const paralleling_monoid&other) {
+    if (*this < other) {
+      return operator =(other);
+    }
+    return *this;
+  }
+
+  bool operator <(const paralleling_monoid&other) const {
+    if (other.iterator) {
+      if (iterator) {
+	return *iterator < other.iterator;
+      }
+      return true;
+    }
+    return false;
+  }
+};
+
+#endif
---
+++ stack_monoid.hpp
@@ -0,0 +1,61 @@
+#ifndef STACK_MONOID_HEADER
+#define STACK_MONOID_HEADER
+
+#include <cassert>
+
+#include "monoid_sequence.hpp"
+
+/* An element of the stack monoid represents a sequence of pushes and pops with
+ * symbol type T.  Pops are particular, and specify which symbol is meant to be
+ * popped.  In case of a mismatch, either the pop is ignored or the topmost
+ * symbol is discarded and the pop retried.  The latter happens if, letting X be
+ * the symbol to pop and Y be the actual top symbol, X.trumps(Y) returns true.
+ *
+ * This monoid has no natural ordering, and therefore cannot be used with
+ * monoid_sequence except in a product monoid.
+ */
+template<typename T>class stack_monoid {
+protected:
+  std::vector<T>			pops;
+  std::vector<T>			pushes;
+
+public:
+  stack_monoid(unsigned zero) { assert(!zero); }
+  stack_monoid(const T&symbol, bool is_pop) {
+    if (is_pop) {
+      pops.push_back(left_delimiter);
+    } else {
+      pushes.push_back(left_delimiter);
+    }
+  }
+
+  stack_monoid operator +(const stack_monoid&other) const {
+    stack_monoid result{*this};
+    return result += other;
+  }
+
+  stack_monoid&operator +=(const stack_monoid&other) {
+    vector<T>::const_iterator
+      pops_begin = other.pops.begin(),
+      pops_end = other.pops.end();
+    for (; pops_begin != pops_end && pushes.size(); ++pops_begin) {
+      while (*pops_begin != pushes.back()) {
+	if (pops_begin->trumps(pushes.back)) {
+	  pushes.pop_back();
+	  if (!pushes.size()) {
+	    goto done_with_cancellation;
+	  }
+	} else {
+	  goto ignore_pop;
+	}
+      }
+      pushes.pop_back();
+    ignore_pop:
+    }
+  done_with_cancellation:
+    pops.insert(pops.end(), pops_begin, pops_end);
+    pushes.insert(pushes.end(), other.pushes.begin(), other.pushes.end());
+  }
+};
+
+#endif
